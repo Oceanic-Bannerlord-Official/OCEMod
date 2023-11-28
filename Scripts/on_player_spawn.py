44de2c31055d2548e122d6c05077d58d41fb0793
Jonny-Havoc <116952614+Jonny-Havoc@users.noreply.github.com>
2023-11-29 08:34:08 +1300

    Update on_player_spawn.py

--- Scripts/on_player_spawn.py
+++ Scripts/on_player_spawn.py
@@ -1,4 +1,10 @@
 #this here tells server to spawn in dem colors G 
 def on_player_spawn(player):
-    faction_colors = get_faction_colors(player.clan.tag)
+    # Get the player's clan tag
+    clan_tag = player.clan.tag
+
+    # Determine the faction colors based on the clan tag
+    faction_colors = get_faction_colors(clan_tag)
+
+    # Set the player's faction colors
     player.faction.set_colors(faction_colors)
