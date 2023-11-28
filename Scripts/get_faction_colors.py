1430ac0623c7b65846903f7c753f24693c63469d
Jonny-Havoc <116952614+Jonny-Havoc@users.noreply.github.com>
2023-11-29 08:20:22 +1300

    Update get_faction_colors.py

--- Scripts/get_faction_colors.py
+++ Scripts/get_faction_colors.py
@@ -1,8 +1,13 @@
 def get_faction_colors(clan_tag):
-#add clan colours ere
+#add clan colours here
     if clan_tag == "WGWM":
         return (0, 255, 0) 
     elif clan_tag == "Jims":
-        return (0, 0, 255)  # Blue
+        return (0, 0, 255)
     else:
-        return (255, 255, 255)  # White
+        return (255, 255, 255)
+
+#this here tells server to spawn in dem colors G 
+def on_player_spawn(player):
+    faction_colors = get_faction_colors(player.clan.tag)
+    player.faction.set_colors(faction_colors)
