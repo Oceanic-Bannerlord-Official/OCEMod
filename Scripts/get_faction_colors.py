23f4e38c230b35c4ee48370fe26963a2cc58bf67
Jonny-Havoc <116952614+Jonny-Havoc@users.noreply.github.com>
2023-11-29 08:17:09 +1300

    Create get_faction_colors.py

---
+++ Scripts/get_faction_colors.py
@@ -0,0 +1,8 @@
+def get_faction_colors(clan_tag):
+#add clan colours ere
+    if clan_tag == "WGWM":
+        return (0, 255, 0) 
+    elif clan_tag == "Jims":
+        return (0, 0, 255)  # Blue
+    else:
+        return (255, 255, 255)  # White
