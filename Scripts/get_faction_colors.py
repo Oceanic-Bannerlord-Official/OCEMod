6e805e1e6240fd87c1d4317c65129d85a298aa68
Jonny-Havoc <116952614+Jonny-Havoc@users.noreply.github.com>
2023-11-29 08:30:37 +1300

    Update get_faction_colors.py

--- Scripts/get_faction_colors.py
+++ Scripts/get_faction_colors.py
@@ -6,8 +6,3 @@ def get_faction_colors(clan_tag):
         return (0, 0, 255)
     else:
         return (255, 255, 255)
-
-#this here tells server to spawn in dem colors G 
-def on_player_spawn(player):
-    faction_colors = get_faction_colors(player.clan.tag)
-    player.faction.set_colors(faction_colors)
